8d0c233df535b40428cd475c7b7319cc740f36ba
mint <mint@mint-PC>
2015-08-13 22:08:08 +0500

    yii2 init

--- basic/config/web.php
+++ basic/config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'futjHuPjQB3VKkkBaqwydP-bZKu2KDhA',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
